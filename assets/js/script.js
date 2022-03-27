6eac9b1bc17d3833b0e442409361f5c889e377db
pigeonsa1t <bowen_samantha@yahoo.com>
2022-03-27 16:00:40 -0400

    game now starts and cycles through the questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // GLOBAL VARIABLES //
 var startButton = document.getElementById("start-button");
-var index = 0;
+// I DONT REALLY UNDERSTAND WHAT THIS DOES:
+var i = 0;
 var score = 0;
 
 // EXPLAINING THE CONTENT OF EACH QUESTION IN THE QUIZ //
@@ -24,32 +25,49 @@ var questionsArray = [
 
 
 // EVERYTHING UNDER THIS LINE WAS DONE DURING TUTOR SESSION 3.26.22 //
+
+// THIS LISTENER WILL HIDE THE START PAGE&SHOW THE FIRST QUESTION WHEN THE BUTTON IS CLICKED
 startButton.addEventListener("click", function(){
+// HIDE THE START PAGE:
     var startElement = document.getElementById("start");
     startElement.setAttribute("class", "hide")
+// SHOW THE QUESTIONS:
     var quizElement = document.getElementById("quiz");
     quizElement.removeAttribute("class");
+// CALL THE FUNCTION TO START BUILDING THE QUESTIONS:
     buildQuestion();
 })
+
+// THIS FUNCTION WILL DISPLAY A QUESTION FOR THE USER TO ANSWER //
 function buildQuestion() {
+// THIS WILL DISPLAY THE TEXT ATTRIBUTE OF THE QUESTIONS ARRAY //
     var quizTitle = document.getElementById("question-title");
-    quizTitle.textContent = questionsArray[index].text;
+    quizTitle.textContent = questionsArray[i].text;
+// !!!!!! I DONT KNOW WHAT THIS DOES
     var buttonBox = document.getElementById("button-box");
     buttonBox.innerHTML = "";
-    questionsArray[index].choices.forEach(function(choice) {
+// THIS LOCAL FUNCTION  WILL RUN THROUGH QUESTIONS ARRAY UNTIL ALL ASKED AND ANSWERED //
+    questionsArray[i].choices.forEach(function(choice) {
         var button = document.createElement("button");
+// THIS DECLARES THAT WHEN THE USER CLICKS A BUTTON TO MAKE A CHOICE...
         button.textContent = choice;
+// THIS SETS AN ATTRIBUTE "VALUE" WHICH IS EQUAL TO THE CHOICE THE USER MADE
         button.setAttribute("value", choice)
+// THE USER MAKES THE CHOICE BASED 
         button.onclick = function() {
             console.log(this.value)
-            if (this.value === questionsArray[index].answer) {
+            console.log(questionsArray[i].answer)
+
+            if (this.value == questionsArray[i].answer) {
                 console.log("right");
             }
             else {
                 console.log("wrong");
             }
-        index++;
-            if(index === questionsArray.length) {
+
+            i++;
+
+            if (i === questionsArray.length) {
                 console.log("end game");
             }
             else {
