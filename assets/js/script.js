a8ef6a013e12a401f89605f67793d1dc2b0f953d
pigeonsa1t <bowen_samantha@yahoo.com>
2022-03-26 22:46:41 -0400

    updating notes to remind myself of what's done during tutor, vs what ive started doing on my own

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
-// startButton
-var startButton = document.getElementById("start-button")
+// GLOBAL VARIABLES //
+var startButton = document.getElementById("start-button");
 var index = 0;
-console.log(startButton)
-// questionsArray [{}]
+var score = 0;
+
+// EXPLAINING THE CONTENT OF EACH QUESTION IN THE QUIZ //
 var questionsArray = [
     {
         text: "question one",
@@ -20,6 +21,15 @@ var questionsArray = [
         answer: 3
     }, 
 ]
+
+for(var i=0; i< questionsArray.length; i++) {
+
+}
+
+
+
+
+// EVERYTHING UNDER THIS LINE WAS DONE DURING TUTOR SESSION 3.26.22 //
 startButton.addEventListener("click", function(){
     var startElement = document.getElementById("start");
     startElement.setAttribute("class", "hide")
@@ -44,7 +54,7 @@ function buildQuestion() {
             else {
                 console.log("wrong");
             }
-            index++;
+        index++;
             if(index === questionsArray.length) {
                 console.log("end game");
             }
@@ -61,3 +71,4 @@ var scoreObj = {
     score: 10
 }
 highscores.push(scoreObj)
+// EVERYTHING ABOVE THIS LINE WAS DONE DURING TUTOR SESSION 3.26.22 //
